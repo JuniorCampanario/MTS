a2165cd1cacb2a6f110ce926a1811c316b9e6143
José René Campanario <jose.campanario@serpro.gov.br>
2018-02-10 15:13:52 -0200

    node server

---
+++ chaves/server.js
@@ -0,0 +1,12 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('certificado.key'),
+  cert: fs.readFileSync('certificado.pem')
+};
+
+https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('hello world\n');
+}).listen(8000);
